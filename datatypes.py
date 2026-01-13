c9d729c06df310ae00fc9f45cc46d1d60de6fe1a
TANDO CALVIN <tandocalvin237@gmail.com>
2026-01-13 15:08:59 +0100

    Add files via upload
    Contains simple basic codes to show my understanding of various datatypes wih string methods and manipulation

---
+++ datatypes.py
@@ -0,0 +1,100 @@
+#literal assignment
+
+first = "john"
+last = "smith"
+# print(type(first) == str)
+# print(isinstance(first, str))
+
+#constructor function
+# food = str("rice")
+# print(type(food) == str)
+# print(isinstance(food, str))
+
+#string concatenation
+# first_name = "John"
+# last_name = "Smith"
+# full_name = first_name + " " + last_name
+# print(full_name)S
+#
+# #casting a number to a string
+# year = str(2026)
+# print(year + " " + full_name)
+
+#multiple lines
+# multiline = '''
+# hey there how are you
+# hope your'e doing good
+#                     all good?
+#
+# '''
+# print(multiline)
+
+# me = "I\'m very pleased to be here today! "
+# print(me)
+
+
+#string methods
+# print(first.upper())
+# print(last.upper())
+
+# multiline = '''
+# hey there how are you
+#
+# hope you're doing good
+#                 are you good?
+#
+# '''
+# # print(multiline.title())
+# # print(multiline.upper())
+#
+# print(multiline.replace("doing", "feeling"))
+#
+# print(len(multiline))
+# multiline += '  '
+# multiline = " " + multiline
+# print(len(multiline))
+# print(len(multiline.strip()))
+# print(len(multiline.lstrip()))
+# print(len(multiline.rstrip()))
+
+
+#LET'S BUILD A MENU
+# title = "menu".upper()
+# print(title.center(50, "="))
+# print("Coffee".ljust(45, ".") + "1xaf".rjust(5))
+# print("Rice".ljust(45, ".") + "10xaf".rjust(5))
+# print("Beans".ljust(45, ".") + "15xaf".rjust(5))
+# print("Fruits".ljust(45, ".") + "12xaf".rjust(5))
+
+# #string index value
+# print(last[2])
+
+#some methods produce boolean values
+# print(first.startswith("j"))
+# print(last.startswith("lS"))
+# print(first == last)
+
+#boolean datatypes
+# myValue = False
+# print(myValue)
+
+
+#complex numbers datatypes
+# comp_value = 2 + 3.5j
+# print(comp_value.imag)
+# print(type(comp_value))
+
+
+#math module and some manipulations
+# print(math.pi)
+# print(math.sqrt(64))
+# print(math.ceil(64.67))
+# print(math.floor(64.45))
+# print(math.exp(0))
+
+#casting a string to a number
+# zipcode = "11003"
+# zip_value = int(zipcode)
+# print(type(zip_value))
+# print(zip_value)
+# print(len(zipcode))
